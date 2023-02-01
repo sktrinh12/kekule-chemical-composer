a748ff4bb2a4319461c8d71e7e9206b81be1fbc2
spencer.trinh <spencer.trinh@kinnate.com>
2023-01-31 19:25:31 -0800

    break out into smaller components, using ref to call onClick from parent

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+import React from 'react'
 import ComposerAndViewer from './components/ComposerViewer'
 import { styled } from '@mui/material/styles'
 import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
-import FormControl from '@mui/material/FormControl'
-import FilledInput from '@mui/material/FilledInput'
-import InputLabel from '@mui/material/InputLabel'
+// import { SMILESProvider } from './SMILESContext'
+import SMILESForm from './components/SMILESForm'
+import Button from '@mui/material/Button'
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(3),
-  maxWidth: 800,
+  maxWidth: 1000,
   color: theme.palette.text.primary,
 }))
 
@@ -21,40 +22,82 @@ const Header = styled('header')(({ theme }) => ({
   marginBottom: theme.spacing,
   backgroundColor: '#282c34',
   justifyContent: 'center',
-  fontSize: '2em',
+  fontSize: '4em',
   lineHeight: 2,
   color: '#D8F3FD',
 }))
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      smilesString: '',
+    }
+    this.composer = React.createRef()
+    this.handleChangeSmiles = this.handleChangeSmiles.bind(this)
+    this.handleSendSmiles = this.handleSendSmiles.bind(this)
+    this.onComposerSMILESClick = this.onComposerSMILESClick.bind(this)
+  }
+  handleChangeSmiles = (event) => {
+    this.setState({ smilesString: event.target.value })
+  }
+  handleSendSmiles = (event) => {
+    console.log(this.state.smilesString)
+  }
 
-function App() {
-  return (
-    <>
-      <Header>Kinnate Molecule Editor</Header>
-      <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 3 }}>
-        <StyledPaper
-          sx={{
-            my: 1,
-            mx: 'auto',
-            p: 2,
-          }}
-        >
-          <Grid container wrap='nowrap' spacing={2}>
-            <Grid item xs={12}>
-              <ComposerAndViewer></ComposerAndViewer>
+  onComposerSMILESClick = () => {
+    this.composer.current.onComposerSMILESClick()
+  }
+
+  render() {
+    return (
+      <>
+        {/*<SMILESProvider value={this.state.smilesString}>*/}
+        <Header>Kinnate Molecule Editor</Header>
+        <Box sx={{ flexGrow: 1 }}>
+          <StyledPaper
+            sx={{
+              my: 1,
+              mx: 'auto',
+              p: 2,
+            }}
+          >
+            <Grid container wrap='nowrap' spacing={2}>
+              <Grid item xs={12}>
+                <ComposerAndViewer
+                  ref={this.composer}
+                  smilesString={this.state.smilesString}
+                ></ComposerAndViewer>
+                {this.state.smilesString}
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <FormControl fullWidth sx={12} variant='filled'>
-                <InputLabel htmlFor='filled-adornment-amount'>
-                  SMILES
-                </InputLabel>
-                <FilledInput id='filled-adornment-amount' />
-              </FormControl>
+          </StyledPaper>
+          <StyledPaper
+            sx={{
+              my: 1,
+              mx: 'auto',
+              p: 2,
+            }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <SMILESForm handleChangeSmiles={this.handleChangeSmiles} />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  size='large'
+                  variant='contained'
+                  onClick={this.onComposerSMILESClick}
+                >
+                  Fetch
+                </Button>
+              </Grid>
             </Grid>
-          </Grid>
-        </StyledPaper>
-      </Box>
-    </>
-  )
+          </StyledPaper>
+        </Box>
+        {/*</SMILESProvider>*/}
+      </>
+    )
+  }
 }
 
 export default App
