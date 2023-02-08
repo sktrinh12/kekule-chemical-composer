b881e76e1ea961e21861c5b1af023e4d1f67fc90
sktrinh12 <sktrinh12@gmail.com>
2023-02-08 01:09:43 -0800

    clear composer on button click

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,153 +1,154 @@
-import React from 'react'
-import ComposerAndViewer from './components/ComposerViewer'
-import { createTheme, styled, ThemeProvider } from '@mui/material/styles'
-import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
-import Grid from '@mui/material/Grid'
-import SMILESForm from './components/SMILESForm'
-import DropDown from './components/Dropdown'
-import Button from '@mui/material/Button'
-import axios from 'axios'
-import XMLParser from 'react-xml-parser'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell, { tableCellClasses } from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import ReactLoading from 'react-loading'
-import Modal from '@mui/material/Modal'
+import React from "react";
+import ComposerAndViewer from "./components/ComposerViewer";
+import { createTheme, styled, ThemeProvider } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import SMILESForm from "./components/SMILESForm";
+import DropDown from "./components/Dropdown";
+import Button from "@mui/material/Button";
+import axios from "axios";
+import XMLParser from "react-xml-parser";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import ReactLoading from "react-loading";
+import Modal from "@mui/material/Modal";
 
-const calcPropsURL = 'http://localhost:5500'
-const blueColour = '#282c34'
+const calcPropsURL = "http://localhost:5500";
+const blueColour = "#282c34";
 const options = {
-  'Standalone Calc Props': 'STANDALONE_CALCULATED_PROPERTIES_TABLE',
-  'Standalone QSAR': 'STANDALONE_QSAR',
-}
+  "Standalone Calc Props": "STANDALONE_CALCULATED_PROPERTIES_TABLE",
+  "Standalone QSAR": "STANDALONE_QSAR",
+};
 
 const themeColour = createTheme({
   palette: {
     blue: {
       main: blueColour,
-      contrastText: '#fff',
+      contrastText: "#fff",
     },
   },
-})
+});
 
 const StyledModal = styled(Modal)({
-  backgroundColor: 'transparent',
-  top: '50%',
-  left: '50%',
-  width: '500px',
-  height: 'auto',
-  marginLeft: '-250px',
-  marginTop: '-150px',
+  backgroundColor: "transparent",
+  top: "50%",
+  left: "50%",
+  width: "500px",
+  height: "auto",
+  marginLeft: "-250px",
+  marginTop: "-150px",
   p: 4,
-})
+});
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(3),
   maxWidth: 1000,
   color: theme.palette.text.primary,
-}))
+}));
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: blueColour,
     color: theme.palette.common.white,
-    alignItems: 'center',
+    alignItems: "center",
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
   },
-}))
+}));
 
-const Header = styled('header')(({ theme }) => ({
+const Header = styled("header")(({ theme }) => ({
   padding: theme.spacing(4),
-  display: 'flex',
+  display: "flex",
   marginBottom: theme.spacing(1),
   backgroundColor: blueColour,
-  justifyContent: 'center',
-  fontSize: '4em',
+  justifyContent: "center",
+  fontSize: "4em",
   lineHeight: 2,
-  color: '#D8F3FD',
+  color: "#D8F3FD",
   fontFamily:
-    '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
-}))
+    "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+}));
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      smilesString: '',
+      smilesString: "",
       data: [],
       loading: false,
       showTable: false,
       open: false,
       selectedIndex: 0,
-      endpoint: options['Standalone Calc Props'],
+      endpoint: options["Standalone Calc Props"],
       insertSmiles: false,
-    }
-    this.composer = React.createRef()
-    this.anchorRef = React.createRef(null)
-    this.handleChangeSmiles = this.handleChangeSmiles.bind(this)
-    this.handleClearSMILES = this.handleClearSMILES.bind(this)
-    this.onComposerSMILESClick = this.onComposerSMILESClick.bind(this)
+    };
+    this.composer = React.createRef();
+    this.anchorRef = React.createRef(null);
+    this.handleChangeSmiles = this.handleChangeSmiles.bind(this);
+    this.handleClearSMILES = this.handleClearSMILES.bind(this);
+    this.onComposerSMILESClick = this.onComposerSMILESClick.bind(this);
   }
   handleChangeSmiles = (event) => {
-    this.setState({ smilesString: event.target.value, insertSmiles: true })
-  }
+    this.setState({ smilesString: event.target.value, insertSmiles: true });
+  };
 
   onComposerSMILESClick = () => {
-    this.composer.current.onComposerSMILESClick()
-  }
+    this.composer.current.onComposerSMILESClick();
+  };
   handleClearSMILES = () => {
-    this.setState({ smilesString: '', chemObj: null })
-  }
+    this.setState({ smilesString: "" });
+    this.composer.current.onComposerClearChemObj();
+  };
   handleUpdateSMILES = (smiles) => {
     // console.log(`calling from Update SMILES: ${smiles}`)
-    this.setState({ smilesString: smiles })
-  }
+    this.setState({ smilesString: smiles });
+  };
   fetchCalcProps = async (smilesString) => {
-    const url = `${calcPropsURL}?smiles=${smilesString}&endpoint=${this.state.endpoint}`
-    console.log(url)
-    this.setState({ loading: true })
+    const url = `${calcPropsURL}?smiles=${smilesString}&endpoint=${this.state.endpoint}`;
+    console.log(url);
+    this.setState({ loading: true });
     try {
       const response = await axios.get(url, {
-        method: 'GET',
-      })
-      let data = await response.data
-      data = new XMLParser().parseFromString(data)
-      console.log(data.children)
-      this.setState({ data: data.children, loading: false, showTable: true })
+        method: "GET",
+      });
+      let data = await response.data;
+      data = new XMLParser().parseFromString(data);
+      console.log(data.children);
+      this.setState({ data: data.children, loading: false, showTable: true });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   handleDropDownClick = (event, index) => {
-    console.info(`You clicked ${Object.keys(options)[index]}`)
+    console.info(`You clicked ${Object.keys(options)[index]}`);
     this.setState({
       open: false,
       selectedIndex: index,
       endpoint: options[Object.keys(options)[index]],
-    })
-  }
+    });
+  };
 
   handleDropDownToggle = () => {
-    this.setState({ open: !this.state.open })
-  }
+    this.setState({ open: !this.state.open });
+  };
 
   handleDropDownClose = (event) => {
     if (
       this.anchorRef.current &&
       this.anchorRef.current.contains(event.target)
     ) {
-      return
+      return;
     }
-    this.setState({ open: false })
-  }
+    this.setState({ open: false });
+  };
 
   render() {
     return (
@@ -157,11 +158,11 @@ class App extends React.Component {
           <StyledPaper
             sx={{
               my: 1,
-              mx: 'auto',
+              mx: "auto",
               p: 2,
             }}
           >
-            <Grid container wrap='nowrap' spacing={2}>
+            <Grid container wrap="nowrap" spacing={2}>
               <Grid item xs={12}>
                 <ComposerAndViewer
                   ref={this.composer}
@@ -176,7 +177,7 @@ class App extends React.Component {
           <StyledPaper
             sx={{
               my: 1,
-              mx: 'auto',
+              mx: "auto",
               p: 2,
             }}
           >
@@ -190,9 +191,9 @@ class App extends React.Component {
               <Grid item xs={2}>
                 <ThemeProvider theme={themeColour}>
                   <Button
-                    color='blue'
-                    size='large'
-                    variant='contained'
+                    color="blue"
+                    size="large"
+                    variant="contained"
                     onClick={this.onComposerSMILESClick}
                   >
                     Fetch
@@ -202,9 +203,9 @@ class App extends React.Component {
               <Grid item xs={4}>
                 <ThemeProvider theme={themeColour}>
                   <Button
-                    color='blue'
-                    size='large'
-                    variant='outlined'
+                    color="blue"
+                    size="large"
+                    variant="outlined"
                     onClick={this.handleClearSMILES}
                   >
                     Clear
@@ -213,9 +214,9 @@ class App extends React.Component {
               </Grid>
               <Grid item xs={6}>
                 <Box
-                  display='flex'
-                  justifyContent='flex-end'
-                  alignItems='flex-end'
+                  display="flex"
+                  justifyContent="flex-end"
+                  alignItems="flex-end"
                 >
                   <DropDown
                     handleDropDownClick={this.handleDropDownClick}
@@ -235,10 +236,10 @@ class App extends React.Component {
             <StyledModal open={true}>
               <>
                 <ReactLoading
-                  type='spin'
+                  type="spin"
                   color={blueColour}
-                  height={'auto'}
-                  width={'100%'}
+                  height={"auto"}
+                  width={"100%"}
                 />
               </>
             </StyledModal>
@@ -248,14 +249,14 @@ class App extends React.Component {
                 <StyledPaper
                   sx={{
                     my: 4,
-                    mx: 'auto',
+                    mx: "auto",
                     p: 2,
                   }}
                 >
                   <Grid container>
                     <Grid item xs={12}>
                       <TableContainer component={Paper}>
-                        <Table aria-label='Property table'>
+                        <Table aria-label="Property table">
                           <TableHead>
                             <TableRow>
                               <StyledTableCell>PROPERTY NAME</StyledTableCell>
@@ -288,8 +289,8 @@ class App extends React.Component {
           )}
         </Box>
       </>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- frontend/src/components/ComposerViewer.js
+++ frontend/src/components/ComposerViewer.js
@@ -1,67 +1,68 @@
-import React from 'react'
-import { Kekule } from 'kekule'
-import 'kekule/theme'
-import { KekuleReact, Components } from '../kekule/kekule.react'
-import '../kekule/ComposerViewer.css'
-const { Molecule } = require('openchemlib')
+import React from "react";
+import { Kekule } from "kekule";
+import "kekule/theme";
+import { KekuleReact, Components } from "../kekule/kekule.react";
+import "../kekule/ComposerViewer.css";
+const { Molecule } = require("openchemlib");
 
-let Composer = Components.Composer
+let Composer = Components.Composer;
 Kekule.environment.setEnvVar(
-  'openbabel.path',
+  "openbabel.path",
   `${process.env.PUBLIC_URL}/kekule/`
-) // need only to call this once
+); // need only to call this once
 // Kekule.OpenBabel.enable((e) => {
 //   console.log(e)
 // })
 
 class ComposerAndViewer extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      composerPredefinedSetting: 'compact',
-      viewerPredefinedSetting: 'basic',
+      composerPredefinedSetting: "compact",
+      viewerPredefinedSetting: "basic",
       chemObj: null,
       selectedObjs: undefined,
       data: {},
-    }
-    this.composer = React.createRef()
+    };
+    this.composer = React.createRef();
 
-    this.onPredefineSettingChange = this.onPredefineSettingChange.bind(this)
-    this.onComposerUserModificationDone =
-      this.onComposerUserModificationDone.bind(this)
-    this.onComposerSelectionChange = this.onComposerSelectionChange.bind(this)
-    this.onComposerSMILESClick = this.onComposerSMILESClick.bind(this)
+    this.onPredefineSettingChange = this.onPredefineSettingChange.bind(this);
+    this.onComposerUserModificationDone = this.onComposerUserModificationDone.bind(
+      this
+    );
+    this.onComposerSelectionChange = this.onComposerSelectionChange.bind(this);
+    this.onComposerSMILESClick = this.onComposerSMILESClick.bind(this);
   }
 
   render() {
-    let selectionInfoElem
+    let selectionInfoElem;
     if (this.state.selectedObjs && this.state.selectedObjs.length) {
       let selDetails = this.getComposerSelectedAtomsAndBonds(
         this.state.selectedObjs
-      )
+      );
       selectionInfoElem = (
         <span>
           You have selected {this.state.selectedObjs.length} object(s),
-          including {selDetails.atoms.length} atom(s) and{' '}
+          including {selDetails.atoms.length} atom(s) and{" "}
           {selDetails.bonds.length} bond(s).
         </span>
-      )
+      );
     } else
       selectionInfoElem = (
         <span>
           Please use the composer to load a chemical structure or draw one
           manually, another option is to input the SMILES string below
         </span>
-      )
+      );
 
     return (
-      <div className='ComposerAndViewerDemo'>
-        <div className='InfoPanel'>
+      <div className="ComposerAndViewerDemo">
+        <div className="InfoPanel">
           <label>{selectionInfoElem}</label>
         </div>
-        <div className='ComposerViewerPair'>
+        <div className="ComposerViewerPair">
           <Composer
-            className='SubWidget'
+            className="SubWidget"
             ref={this.composer}
             predefinedSetting={this.state.composerPredefinedSetting}
             onUserModificationDone={this.onComposerUserModificationDone}
@@ -69,62 +70,67 @@ class ComposerAndViewer extends React.Component {
           ></Composer>
         </div>
       </div>
-    )
+    );
   }
 
   getComposerSelectedAtomsAndBonds(selection) {
-    let result = { atoms: [], bonds: [] }
-    ;(selection || []).forEach((obj) => {
-      if (obj instanceof Kekule.Atom) result.atoms.push(obj)
-      else if (obj instanceof Kekule.Bond) result.bonds.push(obj)
-    })
-    return result
+    let result = { atoms: [], bonds: [] };
+    (selection || []).forEach((obj) => {
+      if (obj instanceof Kekule.Atom) result.atoms.push(obj);
+      else if (obj instanceof Kekule.Bond) result.bonds.push(obj);
+    });
+    return result;
   }
 
   onPredefineSettingChange(e) {
-    this.setState({ composerPredefinedSetting: e.target.value })
+    this.setState({ composerPredefinedSetting: e.target.value });
   }
 
   onComposerUserModificationDone(e) {
-    let composerWidget = this.composer.current.getWidget()
-    this.setState({ chemObj: composerWidget.getChemObj() })
-    let mol = composerWidget.exportObjs(Kekule.Molecule)
+    let composerWidget = this.composer.current.getWidget();
+    this.setState({ chemObj: composerWidget.getChemObj() });
+    let mol = composerWidget.exportObjs(Kekule.Molecule);
     if (mol.length) {
-      let smilesString = Kekule.IO.saveFormatData(mol[0], 'smi')
-      this.props.handleUpdateSMILES(smilesString)
-      console.log(smilesString)
+      let smilesString = Kekule.IO.saveFormatData(mol[0], "smi");
+      this.props.handleUpdateSMILES(smilesString);
+      console.log(smilesString);
     }
   }
   onComposerSelectionChange(e) {
     this.setState({
       selectedObjs: this.composer.current.getWidget().getSelection(),
-    })
+    });
+  }
+
+  onComposerClearChemObj(e) {
+    this.setState({ chemObj: null });
+    this.composer.current.getWidget().newDoc();
+    // console.log(this.composer.current.getWidget());
   }
 
   onComposerSMILESClick(e) {
-    let mol
-    let smilesString
-    smilesString = this.props.smilesString
-    const molfile = Molecule.fromSmiles(smilesString).toMolfile()
-    mol = Kekule.IO.loadFormatData(molfile, 'mol')
+    let mol;
+    let smilesString;
+    smilesString = this.props.smilesString;
+    const molfile = Molecule.fromSmiles(smilesString).toMolfile();
+    mol = Kekule.IO.loadFormatData(molfile, "mol");
     // console.log(this.props.insertSmiles)
     if (this.props.insertSmiles) {
-      let generator = new Kekule.Calculator.ObStructure2DGenerator()
-      generator.setSourceMol(mol)
-      console.log('test')
+      let generator = new Kekule.Calculator.ObStructure2DGenerator();
+      generator.setSourceMol(mol);
       generator.execute((err) => {
         if (!err) {
-          let newMol = generator.getGeneratedMol()
-          this.composer.current.getWidget().setChemObj(newMol)
+          let newMol = generator.getGeneratedMol();
+          this.composer.current.getWidget().setChemObj(newMol);
           // this.setState({ chemObj: newMol })
         } else {
-          console.error(err)
+          console.error(err);
         }
-      })
+      });
     }
-    console.log(smilesString)
-    this.props.fetchCalcProps(smilesString)
+    console.log(smilesString);
+    this.props.fetchCalcProps(smilesString);
   }
 }
 
-export default ComposerAndViewer
+export default ComposerAndViewer;
